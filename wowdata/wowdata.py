1211f41746cb77d74a15cf9496f8f1ee90b40f06
Laird Streak <LairdStreak@gmail.com>
2018-03-22 16:45:16 +1300

    Use Chrome vs Phantom add code.

--- wowdata/wowdata.py
+++ wowdata/wowdata.py
@@ -1,1 +1,26 @@
+'''
+Module Docstring
+'''
+import pprint as pp
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
+
+def main_function():
+    '''Function Doc String
+    '''
+    url = "http://www.wowhead.com/world-quests/na"
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=0x0')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(url)
+    content_data = driver.page_source
+    driver.close()
+    driver.quit()
+    soup = BeautifulSoup(content_data, "html.parser")
+    pp.pprint(soup.prettify)
+
+
+if __name__ == '__main__':
+    main_function()
