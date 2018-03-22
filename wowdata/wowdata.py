24a0f3f31973ae01d5a2b0b7562a6a65bb722a98
Laird Streak <LairdStreak@gmail.com>
2018-03-22 20:38:03 +1300

    Update code will continue

--- wowdata/wowdata.py
+++ wowdata/wowdata.py
@@ -9,11 +9,17 @@ from bs4 import BeautifulSoup
 def main_function():
     '''Function Doc String
     '''
+    records = fetch_wowhead_dailies()
+
+
+def fetch_wowhead_dailies():
+    '''docstr
+    '''
     url = "http://www.wowhead.com/world-quests/na"
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=0x0')
-    driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome(chrome_options=options, executable_path=r"chromedriver.exe")
     driver.get(url)
     content_data = driver.page_source
     driver.close()
@@ -21,17 +27,16 @@ def main_function():
     soup = BeautifulSoup(content_data, "html.parser")
     div = soup.find(id="articlebody")
     listdiv = soup.find(id='list')
-    table_item = soup.select('#lv-lv-world-quests > div.listview-scroller > table > tbody')
-
-    rows = table_item.find("tbody").find_all("tr")
-
-    for row in rows:
-        cells = row.find_all("td")
-        rn = cells[0].get_text()
-
-    pp.pprint(listdiv)
-    pp.pprint(listdiv)
-
+    table_item = soup.select('#lv-lv-world-quests > div.listview-scroller > table > tbody > tr')
+    arrMain = []
+    for record in table_item:
+        arrSub = []
+        for index, itemCell in enumerate(record.children):
+            arrSub.insert(index, itemCell.text)
+            print("{} {}".format(index, itemCell.text))
+        arrMain.append(arrSub)
+    
+    return arrMain
 
 if __name__ == '__main__':
     main_function()
